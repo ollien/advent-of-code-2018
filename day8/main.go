838ee4022eef5d9a134c951267a6ac81062c8345
Nick Krichevsky <nick@ollien.com>
2018-12-08 14:07:14 -0500

    Add day 8 part 2 solution

--- day8/main.go
+++ day8/main.go
@@ -8,6 +8,12 @@ import (
 	"strings"
 )
 
+// only used for part 2
+type node struct {
+	value       int
+	childValues []int
+}
+
 func sumUntil(arr []int, max int) int {
 	total := 0
 	for i := 0; i < max; i++ {
@@ -37,9 +43,55 @@ func findTreeTotal(tree []int, numNodes int) (int, int) {
 	return cursor, total
 }
 
+func getChildValuesFromMetadata(tree []int, numValues int, children []node) int {
+	total := 0
+	for i := 0; i < numValues; i++ {
+		childIndex := tree[i] - 1
+		if childIndex < len(children) {
+			total += children[childIndex].value
+		}
+	}
+
+	return total
+}
+
+func calculateRootNode(tree []int, numNodes int) (int, []node) {
+	nodes := make([]node, numNodes)
+	cursor := 0
+	for i := 0; i < numNodes; i++ {
+		numChildren, metadataCount := tree[0], tree[1]
+		// Remove the tree header
+		tree = tree[2:]
+		n, children := calculateRootNode(tree, numChildren)
+		// Remove the part that the subtree parsed
+		tree = tree[n:]
+		if numChildren == 0 {
+			// Extract the value of the metadata
+			nodes[i].value = sumUntil(tree, metadataCount)
+		} else {
+			nodes[i].value = getChildValuesFromMetadata(tree, metadataCount, children)
+			// Copy the values of the children into our node
+			nodes[i].childValues = make([]int, numChildren)
+			for j := 0; j < numChildren; j++ {
+				nodes[i].childValues[j] = children[j].value
+			}
+		}
+		tree = tree[metadataCount:]
+		// Advance the cursor that we will return past the metadata and the subtrees
+		cursor += metadataCount + 2 + n
+	}
+
+	return cursor, nodes
+}
+
 func part1(tree []int) int {
 	_, total := findTreeTotal(tree, 1)
 	return total
+}
+
+func part2(tree []int) int {
+	_, rootedTree := calculateRootNode(tree, 1)
+	return rootedTree[0].value
 }
 
 func main() {
@@ -64,4 +116,5 @@ func main() {
 		tree = append(tree, result)
 	}
 	fmt.Println(part1(tree))
+	fmt.Println(part2(tree))
 }
