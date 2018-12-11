0177531a021417ecb032a087492f46529a647681
Nick Krichevsky <nick@ollien.com>
2018-12-11 12:18:55 -0500

    Add day 11 part 1 solution

---
+++ day11/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+const gridSize = 300
+
+func getSquareScore(startRow int, startCol int, serialNumber int) (score int) {
+	for i := startRow; i < startRow+3; i++ {
+		for j := startCol; j < startCol+3; j++ {
+			rackID := j + 10
+			powerLevel := rackID * (rackID*i + serialNumber)
+			powerLevel = powerLevel / 100 % 10
+			score += powerLevel - 5
+		}
+	}
+
+	return
+}
+
+func part1(serialNumber int) (bestRow int, bestCol int) {
+	maxScore := 0
+	for i := 1; i <= gridSize; i++ {
+		for j := 1; j <= gridSize; j++ {
+			score := getSquareScore(i, j, serialNumber)
+			if score > maxScore {
+				maxScore = score
+				bestRow = i
+				bestCol = j
+			}
+		}
+	}
+
+	return
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./main serial_number")
+		return
+	}
+
+	rawSerialNumber := os.Args[1]
+	serialNumber, err := strconv.Atoi(rawSerialNumber)
+	if err != nil {
+		panic(err)
+	}
+
+	bestRow, bestCol := part1(serialNumber)
+	fmt.Printf("%d,%d\n", bestCol, bestRow)
+}
