7299146f13c9b088bd1e35895b6e7eb8c625b1be
Nick Krichevsky <nick@ollien.com>
2018-12-11 22:16:07 -0500

    Add day 11 part 2 solutoin using a summed area table

--- day11/main.go
+++ day11/main.go
@@ -8,24 +8,79 @@ import (
 
 const gridSize = 300
 
-func getSquareScore(startRow int, startCol int, size int, serialNumber int) (score int) {
-	for i := startRow; i < startRow+size; i++ {
-		for j := startCol; j < startCol+size; j++ {
-			rackID := j + 10
-			powerLevel := rackID * (rackID*i + serialNumber)
-			powerLevel = powerLevel / 100 % 10
-			score += powerLevel - 5
+type summedAreaTable [][]int
+
+// Make a table where each location represents the scores up and to the left
+func makeSummedAreaTable(serialNumber int) summedAreaTable {
+	table := make([][]int, gridSize)
+	for row := range table {
+		table[row] = make([]int, gridSize)
+		for col := range table[row] {
+			score := getTileScore(row+1, col+1, serialNumber)
+			if row-1 >= 0 && col-1 >= 0 {
+				score += table[row-1][col] + table[row][col-1] - table[row-1][col-1]
+			} else if row-1 >= 0 {
+				score += table[row-1][col]
+			} else if col-1 >= 0 {
+				score += table[row][col-1]
+			}
+			table[row][col] = score
 		}
 	}
 
-	return
+	return table
 }
 
-func part1(serialNumber int) (bestRow int, bestCol int) {
+func getTileScore(row int, col int, serialNumber int) int {
+	rackID := col + 10
+	powerLevel := rackID * (rackID*row + serialNumber)
+	powerLevel = powerLevel / 100 % 10
+	return powerLevel - 5
+}
+
+func getSquareScore(areaTable summedAreaTable, startRow int, startCol int, size int, serialNumber int) (score int) {
+	bottomRightRow := startRow + (size - 1)
+	bottomRightCol := startCol + (size - 1)
+	if bottomRightCol >= gridSize || bottomRightRow >= gridSize {
+		return 0
+	}
+
+	bottomRightScore := areaTable[bottomRightRow][bottomRightCol]
+
+	leftBoundRow := bottomRightRow
+	leftBoundCol := startCol - 1
+	leftBoundScore := 0
+	if leftBoundRow >= 0 && leftBoundCol >= 0 {
+		leftBoundScore = areaTable[leftBoundRow][leftBoundCol]
+	}
+
+	topBoundRow := startRow - 1
+	topBoundCol := bottomRightCol
+	topBoundScore := 0
+	if topBoundRow >= 0 && topBoundCol >= 0 {
+		topBoundScore = areaTable[topBoundRow][topBoundCol]
+	}
+
+	topLeftBoundScore := 0
+	if startRow > 0 && startCol > 0 {
+		topLeftBoundScore = areaTable[startRow-1][startCol-1]
+	}
+
+	/*
+	 * cxxb
+	 * xxxx
+	 * xxxx
+	 * dxxa
+	 * Perform a - b - c + d; the +d eliminates double counting form the other two subtractions
+	 */
+	return bottomRightScore - leftBoundScore - topBoundScore + topLeftBoundScore
+}
+
+func part1(areaTable summedAreaTable, serialNumber int) (bestRow int, bestCol int) {
 	maxScore := 0
-	for i := 1; i <= gridSize; i++ {
-		for j := 1; j <= gridSize; j++ {
-			score := getSquareScore(i, j, 3, serialNumber)
+	for i := 1; i < gridSize; i++ {
+		for j := 1; j < gridSize; j++ {
+			score := getSquareScore(areaTable, i-1, j-1, 3, serialNumber)
 			if score > maxScore {
 				maxScore = score
 				bestRow = i
@@ -37,12 +92,12 @@ func part1(serialNumber int) (bestRow int, bestCol int) {
 	return
 }
 
-func part2(serialNumber int) (bestRow int, bestCol int, bestSize int) {
+func part2(areaTable summedAreaTable, serialNumber int) (bestRow int, bestCol int, bestSize int) {
 	maxScore := 0
 	for squareSize := 1; squareSize <= gridSize; squareSize++ {
-		for i := 1; i <= gridSize; i++ {
-			for j := 1; j <= gridSize; j++ {
-				score := getSquareScore(i, j, squareSize, serialNumber)
+		for i := 1; i < gridSize; i++ {
+			for j := 1; j < gridSize; j++ {
+				score := getSquareScore(areaTable, i-1, j-1, squareSize, serialNumber)
 				if score > maxScore {
 					maxScore = score
 					bestRow = i
@@ -68,8 +123,9 @@ func main() {
 		panic(err)
 	}
 
-	bestRow, bestCol := part1(serialNumber)
+	areaTable := makeSummedAreaTable(serialNumber)
+	bestRow, bestCol := part1(areaTable, serialNumber)
 	fmt.Printf("%d,%d\n", bestCol, bestRow)
-	bestRow, bestCol, bestSize := part2(serialNumber)
+	bestRow, bestCol, bestSize := part2(areaTable, serialNumber)
 	fmt.Printf("%d,%d,%d\n", bestCol, bestRow, bestSize)
 }
