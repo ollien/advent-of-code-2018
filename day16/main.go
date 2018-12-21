cb4ff2474368fe341ec4d64c3d9fa271144f7b3a
Nick Krichevsky <nick@ollien.com>
2018-12-20 20:59:10 -0500

    Add day 16 part 2 solution

--- day16/main.go
+++ day16/main.go
@@ -15,6 +15,9 @@ const (
 	afterFormat         = "After: [%d, %d, %d, %d]"
 )
 
+// Can't use a constant for an array - this is our next best thing
+var operations = [...]deviceFunction{addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr}
+
 type registerSet [4]int
 type instruction [4]int
 type deviceFunction = func(registerSet, int, int, int) registerSet
@@ -114,21 +117,38 @@ func eqrr(registers registerSet, register1 int, register2 int, destinationRegist
 	return eqii(registers, registers[register1], registers[register2], destinationRegister)
 }
 
-func (n note) getNumMatchingOperations() (count int) {
-	operations := []deviceFunction{addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr}
-	for _, operation := range operations {
+// Gets the indices in the operations array of the operations that match the note
+func (n note) getMatchingOperations() []int {
+	matchingOperations := []int{}
+	for i, operation := range operations {
 		if operation(n.before, n.input[1], n.input[2], n.input[3]) == n.after {
-			count++
+			matchingOperations = append(matchingOperations, i)
 		}
 	}
-	return
+
+	return matchingOperations
 }
 
-func parsePart1Input(rawNotes []string) ([]note, error) {
+func parseInput(input []string) ([]note, []instruction, error) {
+	notes, rawPart2Input, err := parsePart1Input(input)
+	if err != nil {
+		return nil, nil, err
+	}
+	instructions, err := parsePart2Input(rawPart2Input)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return notes, instructions, nil
+}
+
+func parsePart1Input(rawNotes []string) ([]note, []string, error) {
 	lastLineLength := -1
 	notes := []note{}
 	currentNote := note{}
-	for _, line := range rawNotes {
+	lastIndex := -1
+	for i, line := range rawNotes {
+		lastIndex = i
 		if lastLineLength == 0 && len(line) == 0 {
 			break
 		}
@@ -142,7 +162,7 @@ func parsePart1Input(rawNotes []string) ([]note, error) {
 
 		numMatched, err := fmt.Sscanf(line, beforeFormat, &registers[0], &registers[1], &registers[2], &registers[3])
 		if err == nil && numMatched != 4 {
-			return nil, errors.New(malformedInputError)
+			return nil, nil, errors.New(malformedInputError)
 		} else if err == nil {
 			currentNote.before = registers
 			continue
@@ -150,7 +170,7 @@ func parsePart1Input(rawNotes []string) ([]note, error) {
 
 		numMatched, err = fmt.Sscanf(line, instructionFormat, &ins[0], &ins[1], &ins[2], &ins[3])
 		if err == nil && numMatched != 4 {
-			return nil, errors.New(malformedInputError)
+			return nil, nil, errors.New(malformedInputError)
 		} else if err == nil {
 			currentNote.input = ins
 			continue
@@ -158,27 +178,133 @@ func parsePart1Input(rawNotes []string) ([]note, error) {
 
 		numMatched, err = fmt.Sscanf(line, afterFormat, &registers[0], &registers[1], &registers[2], &registers[3])
 		if err == nil && numMatched != 4 {
-			return nil, errors.New(malformedInputError)
+			return nil, nil, errors.New(malformedInputError)
 		} else if err == nil {
 			currentNote.after = registers
 			notes = append(notes, currentNote)
 		} else {
 			// If we have an error at this point, something is actually wrong.
-			return nil, err
+			return nil, nil, err
 		}
 	}
 
-	return notes, nil
+	return notes, rawNotes[lastIndex+2:], nil
 }
 
-func part1(notes []note) (total int) {
-	for _, note := range notes {
-		numOps := note.getNumMatchingOperations()
-		if numOps >= 3 {
-			total++
+func parsePart2Input(rawInstructions []string) ([]instruction, error) {
+	instructions := make([]instruction, 0, len(rawInstructions))
+	for _, line := range rawInstructions {
+		var ins instruction
+		numMatched, err := fmt.Sscanf(line, instructionFormat, &ins[0], &ins[1], &ins[2], &ins[3])
+		if err != nil {
+			return nil, err
+		} else if numMatched != 4 {
+			return nil, errors.New(malformedInputError)
+		}
+		instructions = append(instructions, ins)
+	}
+
+	return instructions, nil
+}
+
+func addIfNotPresent(src []int, dst []int) []int {
+	resultSlice := make([]int, len(dst))
+	copy(resultSlice, dst)
+	for _, srcItem := range src {
+		found := false
+		for _, dstItem := range dst {
+			if dstItem == srcItem {
+				found = true
+				break
+			}
+		}
+		if !found {
+			resultSlice = append(resultSlice, srcItem)
 		}
 	}
-	return
+
+	return resultSlice
+}
+
+func allListsEmpty(itemMap map[int][]int) bool {
+	for _, itemList := range itemMap {
+		if len(itemList) != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func getOneItemList(itemMap map[int][]int) int {
+	for k, itemList := range itemMap {
+		if len(itemList) == 1 {
+			return k
+		}
+	}
+
+	return -1
+}
+
+func removeItemFromAllLists(itemMap map[int][]int, target int) {
+	for k, itemList := range itemMap {
+		index := -1
+		for i, item := range itemList {
+			if item == target {
+				index = i
+				break
+			}
+		}
+		if index != -1 {
+			itemMap[k] = append(itemMap[k][:index], itemMap[k][index+1:]...)
+		}
+	}
+}
+
+func getOpcodesFromPossibilities(possibleOpcodes map[int][]int) map[int]deviceFunction {
+	opcodes := make(map[int]deviceFunction, len(possibleOpcodes))
+	for !allListsEmpty(possibleOpcodes) {
+		// Find an opcode with only one function as a possibility - that way we know for certain this opcode maps to this function.
+		nextOpcode := getOneItemList(possibleOpcodes)
+		functionID := possibleOpcodes[nextOpcode][0]
+		opcodes[nextOpcode] = operations[functionID]
+		// Remove all other instances of this item from other lists, thus creating another one function mapping.
+		removeItemFromAllLists(possibleOpcodes, functionID)
+	}
+
+	return opcodes
+}
+
+// part1 runs the part1 simulation, and also gives us the possibilities for opcodes, a needed part for part2
+func part1(notes []note) (int, map[int][]int) {
+	opcodeMappings := map[int][]int{}
+	total := 0
+	for _, note := range notes {
+		matchingOperations := note.getMatchingOperations()
+		if len(matchingOperations) >= 3 {
+			total++
+		}
+		opcode := note.input[0]
+		// If an opcode is already stored, we only need to remove the opcodes that weren't already there
+		if _, haveOpcode := opcodeMappings[opcode]; haveOpcode {
+			opcodeMappings[opcode] = matchingOperations
+			opcodeMappings[opcode] = addIfNotPresent(matchingOperations, opcodeMappings[opcode])
+		} else {
+			opcodeMappings[opcode] = matchingOperations
+		}
+	}
+
+	return total, opcodeMappings
+}
+
+func part2(instructions []instruction, opcodes map[int]deviceFunction) int {
+	var registers registerSet
+	for _, instruction := range instructions {
+		opcode := instruction[0]
+		registers = opcodes[opcode](registers, instruction[1], instruction[2], instruction[3])
+	}
+
+	return registers[0]
 }
 
 func main() {
@@ -195,9 +321,13 @@ func main() {
 	rawNotes := strings.Split(string(inFileContents), "\n")
 	// trim trailing newline
 	rawNotes = rawNotes[:len(rawNotes)-1]
-	part1Notes, err := parsePart1Input(rawNotes)
+	part1Notes, part2Instructions, err := parseInput(rawNotes)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(part1(part1Notes))
+
+	part1Result, opcodePossibilities := part1(part1Notes)
+	fmt.Println(part1Result)
+	opcodes := getOpcodesFromPossibilities(opcodePossibilities)
+	fmt.Println(part2(part2Instructions, opcodes))
 }
